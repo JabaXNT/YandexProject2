1b8baed3d8a71a2924a4e60dec63b2e59e0e3c92
JabaXNT <gluhow2015@gmail.com>
2020-12-15 15:00:30 +0500

    сложно

---
+++ src/code/Illness_class.py
@@ -0,0 +1,182 @@
+import sqlite3
+import sys
+import lang
+
+from src.design.illness import Ui_Form_1
+from src.design.add_ill import Ui_Dialog_3
+from src.design.changeill_card import Ui_Dialog
+from PyQt5.QtWidgets import QApplication, QTableWidget, QDialog, QWidget, QMenu, QTableWidgetItem, QMessageBox
+from PyQt5 import QtCore, QtGui
+
+
+class ChangeIll(QDialog, Ui_Dialog):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.main = parent
+        self.setupUi(self)
+        self.setWindowTitle(lang.cards_input)
+        self.connection = sqlite3.connect('Health_cards.sqlite')
+        self.res = self.connection.cursor()
+        self.saveIll_btn.clicked.connect(self.save)
+        self.diag_new.setText(self.main.infoill[0])
+        self.diag_id.setText(self.main.id_numb[0])
+
+    def save(self):
+        self.main.diagIll_new = ''.join(self.diag_new.text().strip())
+        self.main.diagID_new = ''.join(self.diag_id.text().strip())
+        if self.main.diagIll_new == '':
+            self.label.setText(lang.diag_input)
+            return
+        if self.main.diagID_new == '':
+            self.label.setText(lang.id_input)
+            return
+        variableill = [self.main.diagID_new, self.main.diagIll_new, self.main.illres[0][0]]
+        self.res.execute("UPDATE Illness SET name_id = ?, illness = ? WHERE id = ?", variableill)
+        self.label.setText('')
+        self.connection.commit()
+        self.connection.close()
+        self.close()
+
+
+class AddIll(QDialog, Ui_Dialog_3):  # Добавление диагноза в БД пациента
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.main = parent
+        self.setWindowTitle('Добавьте диагноз')
+        self.setupUi(self)
+        self.connection = sqlite3.connect('Health_cards.sqlite')
+        self.res = self.connection.cursor()
+        self.okIll_btn.clicked.connect(self.save_ill)
+        self.flag = True
+
+    def save_ill(self):
+        self.flag = True
+        self.main.idIll = ''.join(self.id.text().strip())
+        self.main.diagIll = ''.join(self.diag.text().strip())
+        if self.main.idIll == '':
+            self.label.setText(lang.id_input)
+            return
+        if self.main.diagIll == '':
+            self.label.setText(lang.diag_input)
+            return
+        result = self.res.execute("SELECT id FROM Patients").fetchall()
+        try:
+            for elem in result:
+                if int(self.main.idIll) == int(elem[0]):
+                    self.flag = False
+                    break
+        except ValueError:
+            self.label.setText(lang.correctid_input)
+        if self.flag:
+            self.label.setText(lang.noid_input)
+            return
+        variableill = [self.main.idIll, self.main.diagIll]
+        self.res.execute("INSERT INTO Illness(name_id, illness) VALUES(?, ?)", variableill)
+        self.label.setText('')
+        self.connection.commit()
+        self.connection.close()
+        self.close()
+
+
+class IllnessCard(QWidget, Ui_Form_1):  # БД с болезнями пациентов
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('История болезни')
+        self.connection = sqlite3.connect('Health_cards.sqlite')
+        self.res = self.connection.cursor()
+        menuill = QMenu()
+        menuill.addAction('ID', self.action1)
+        menuill.addAction('Болезнь', self.action2)
+        self.chsill_btn.setMenu(menuill)
+        self.chngill_btn.clicked.connect(self.ill_change)
+        self.addill_btn.clicked.connect(self.ill_add)
+        self.srchill_btn.clicked.connect(self.ill_search)
+        self.showill_btn.clicked.connect(self.ill_show)
+        self.delill_btn.clicked.connect(self.ill_delete)
+        self.bruh = []
+        self.db = 'name_id'
+        self.ill_show()
+
+    def action1(self):
+        self.chsill_btn.setText('ID')
+        self.db = 'name_id'
+
+    def action2(self):
+        self.chsill_btn.setText('Болезнь')
+        self.db = 'illness'
+
+    def ill_add(self):
+        self.ill_add = AddIll(self)
+        self.ill_add.show()
+
+    def ill_change(self):
+        self.row_numb = list(set([i.row() for i in self.ill_table.selectedItems()]))
+        self.id_numb = [self.ill_table.item(i, 0).text() for i in self.row_numb]
+        self.infoill = [self.ill_table.item(i, 1).text() for i in self.row_numb]
+        try:
+            self.bruh = self.id_numb[0], self.infoill[0]
+        except IndexError:
+            self.label_msg.setText(lang.takecard)
+            return
+        self.illres = self.res.execute("SELECT id FROM Illness WHERE name_id = ? AND illness = ?",
+                                       self.bruh).fetchall()
+        if len(self.id_numb) == 0:
+            self.label_msg.setText(lang.takecard)
+            return
+        self.changeill_card = ChangeIll(self)
+        self.label_msg.setText('')
+        self.changeill_card.show()
+
+    def ill_delete(self):
+        rows = list(set([i.row() for i in self.ill_table.selectedItems()]))
+        ids = [self.ill_table.item(i, 0).text() for i in rows]
+        ibs = [self.ill_table.item(i, 1).text() for i in rows]
+        if len(ids) > 1:
+            self.label_msg.setText(lang.maxdel_card)
+            return
+        result = ids + ibs
+        if len(result) == 0:
+            self.label_msg.setText(lang.del_card)
+            return
+        use = self.res.execute("SELECT id FROM Illness WHERE name_id = ? AND illness = ?", result).fetchall()
+        if len(ids) > 0:
+            sure = QMessageBox.question(
+                self, '', "Действительно удалить карту?"
+                , QMessageBox.Yes, QMessageBox.No)
+            if sure == QMessageBox.Yes:
+                self.res.execute("DELETE FROM Illness WHERE id = ?", use[0])
+                self.label_msg.setText('')
+        self.connection.commit()
+        self.ill_show()
+
+    def ill_search(self):  # Поиск карт по параметрам
+        if self.db == 'name_id':
+            try:
+                result = self.res.execute(
+                    f"SELECT name_id, illness FROM Illness WHERE {self.db} = {self.lineEdit.text()}").fetchall()
+            except sqlite3.OperationalError:
+                return
+        else:
+            try:
+                result = self.res.execute(
+                    f"SELECT name_id, illness FROM Illness "
+                    f"WHERE {self.db} LIKE '{self.lineEdit.text().capitalize()}%'").fetchall()
+            except sqlite3.OperationalError:
+                return
+        if not result:
+            self.label_msg.setText(lang.nocardinfo)
+            return
+        else:
+            self.ill_table.setRowCount(len(result))
+            for i, elem in enumerate(result):
+                for j, val in enumerate(elem):
+                    self.ill_table.setItem(i, j, QTableWidgetItem(str(val)))
+
+    def ill_show(self):
+        result = self.res.execute("SELECT name_id, illness FROM Illness").fetchall()
+        self.ill_table.setRowCount(len(result))
+        self.ill_table.setColumnCount(len(result[0]))
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.ill_table.setItem(i, j, QTableWidgetItem(str(val)))
---
+++ src/code/Treatment_class.py
@@ -0,0 +1,180 @@
+import sqlite3
+import sys
+import lang
+
+from src.design.treatment import Ui_Form_2
+from src.design.add_trt import Ui_Dialog_4
+from src.design.changetrt_card import Ui_Dialog
+from PyQt5.QtWidgets import QApplication, QTableWidget, QDialog, QWidget, QMenu, QTableWidgetItem, QMessageBox
+from PyQt5 import QtCore, QtGui
+
+
+class ChangeTrt(QDialog, Ui_Dialog):
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.main = parent
+        self.setupUi(self)
+        self.setWindowTitle(lang.cards_input)
+        self.connection = sqlite3.connect('Health_cards.sqlite')
+        self.res = self.connection.cursor()
+        self.saveTrt_btn.clicked.connect(self.save)
+        self.cure_new.setText(self.main.infotrt[0])
+        self.cure_id.setText(self.main.id_numb[0])
+
+    def save(self):
+        self.main.cureTrt_new = ''.join(self.cure_new.text().strip())
+        self.main.cureID_new = ''.join(self.cure_id.text().strip())
+        if self.main.cureTrt_new == '':
+            self.label.setText(lang.treat_input)
+            return
+        if self.main.cureID_new == '':
+            self.label.setText(lang.id_input)
+            return
+        variabletrt = [self.main.cureID_new, self.main.cureTrt_new, self.main.trtres[0][0]]
+        self.res.execute("UPDATE Treat SET name_id = ?, medication = ? WHERE id = ?", variabletrt)
+        self.label.setText('')
+        self.connection.commit()
+        self.connection.close()
+        self.close()
+
+
+class AddTrt(QDialog, Ui_Dialog_4):  # Добавление лекарства в БД пациента
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        self.main = parent
+        self.setupUi(self)
+        self.setWindowTitle('Добавьте лекарство')
+        self.connection = sqlite3.connect('Health_cards.sqlite')
+        self.res = self.connection.cursor()
+        self.okTrt_btn.clicked.connect(self.save_trt)
+        self.flag = True
+
+    def save_trt(self):
+        self.flag = True
+        self.main.idTrt = ''.join(self.id.text().strip())
+        self.main.cureTrt = ''.join(self.cure.text().strip())
+        if self.main.idTrt == '':
+            self.label.setText(lang.id_input)
+            return
+        if self.main.cureTrt == '':
+            self.label.setText(lang.treat_input)
+            return
+        result = self.res.execute("SELECT id FROM Patients").fetchall()
+        try:
+            for elem in result:
+                if int(self.main.idTrt) == int(elem[0]):
+                    self.flag = False
+                    break
+        except ValueError:
+            self.label.setText(lang.correctid_input)
+        if self.flag:
+            self.label.setText(lang.noid_input)
+            return
+        variable3 = [self.main.idTrt, self.main.cureTrt]
+        self.res.execute("INSERT INTO Treat(name_id, medication) VALUES(?, ?)", variable3)
+        self.label.setText('')
+        self.connection.commit()
+        self.connection.close()
+        self.close()
+
+
+class TreatmentCard(QWidget, Ui_Form_2):  # История лечения пациента (хз когда доделаю)
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setWindowTitle('Лечение')
+        self.connection = sqlite3.connect('Health_cards.sqlite')
+        self.res = self.connection.cursor()
+        menutrt = QMenu()
+        menutrt.addAction('ID', self.action1)
+        menutrt.addAction('Лекарство', self.action2)
+        self.chstrt_btn.setMenu(menutrt)
+        self.deltrt_btn.clicked.connect(self.trt_delete)
+        self.chngtrt_btn.clicked.connect(self.trt_change)
+        self.srchtrt_btn.clicked.connect(self.trt_search)
+        self.showtrt_btn.clicked.connect(self.trt_show)
+        self.addtrt_btn.clicked.connect(self.trt_add)
+        self.bruh = []
+        self.db = 'name_id'
+        self.trt_show()
+
+    def action1(self):
+        self.chstrt_btn.setText('ID')
+        self.db = 'name_id'
+
+    def action2(self):
+        self.chstrt_btn.setText('Лекарство')
+        self.db = 'medication'
+
+    def trt_add(self):
+        self.trt_add = AddTrt(self)
+        self.trt_add.show()
+
+    def trt_change(self):
+        self.row_numb = list(set([i.row() for i in self.trt_table.selectedItems()]))
+        self.id_numb = [self.trt_table.item(i, 0).text() for i in self.row_numb]
+        self.infotrt = [self.trt_table.item(i, 1).text() for i in self.row_numb]
+        try:
+            self.bruh = self.id_numb[0], self.infotrt[0]
+        except IndexError:
+            self.label_msg.setText(lang.takecard)
+            return
+        self.trtres = self.res.execute("SELECT id FROM Treat WHERE name_id = ? AND medication = ?",
+                                       self.bruh).fetchall()
+        if len(self.id_numb) == 0:
+            self.label_msg.setText(lang.takecard)
+            return
+        self.changetrt_card = ChangeTrt(self)
+        self.label_msg.setText('')
+        self.changetrt_card.show()
+
+    def trt_delete(self):
+        rows = list(set([i.row() for i in self.trt_table.selectedItems()]))
+        ids = [self.trt_table.item(i, 0).text() for i in rows]
+        ibs = [self.trt_table.item(i, 1).text() for i in rows]
+        if len(ids) > 1:
+            self.label_msg.setText(lang.maxdel_card)
+            return
+        result = ids + ibs
+        if len(result) == 0:
+            self.label_msg.setText(lang.del_card)
+            return
+        use = self.res.execute("SELECT id FROM Treat WHERE name_id = ? AND medication = ?", result).fetchall()
+        if len(ids) > 0:
+            sure = QMessageBox.question(
+                self, '', "Действительно удалить карту?"
+                , QMessageBox.Yes, QMessageBox.No)
+            if sure == QMessageBox.Yes:
+                self.res.execute("DELETE FROM Treat WHERE id = ?", use[0])
+                self.label_msg.setText('')
+        self.connection.commit()
+        self.trt_show()
+
+    def trt_search(self):  # Поиск карт по параметрам
+        if self.db == 'name_id':
+            try:
+                result = self.res.execute(
+                    f"SELECT name_id, medication FROM Treat WHERE {self.db} = {self.lineEdit.text()}").fetchall()
+            except sqlite3.OperationalError:
+                return
+        else:
+            result = self.res.execute(
+                f"SELECT name_id, medication FROM Treat"
+                f" WHERE {self.db} LIKE '{self.lineEdit.text().capitalize()}%'").fetchall()
+        if not result:
+            self.label_msg.setText(lang.nocardinfo)
+            return
+        else:
+            self.trt_table.setRowCount(len(result))
+            self.label_msg.setText('')
+            for i, elem in enumerate(result):
+                for j, val in enumerate(elem):
+                    self.trt_table.setItem(i, j, QTableWidgetItem(str(val)))
+
+    def trt_show(self):
+        result = self.res.execute("SELECT name_id, medication FROM Treat").fetchall()
+        self.trt_table.setRowCount(len(result))
+        self.trt_table.setColumnCount(len(result[0]))
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.trt_table.setItem(i, j, QTableWidgetItem(str(val)))
